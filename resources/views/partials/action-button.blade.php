bb9e7aa6467c57e4f3ae677426c9524dd9ccc1d9
MeNavel <information.navel@gmail.com>
2023-02-07 23:12:52 +0700

    benahin gitignore

---
+++ resources/views/partials/action-button.blade.php
@@ -0,0 +1,6 @@
+<div class="table">
+    <a class="btn btn-primary" href="' . route('edit-mundurejo', $row->id) . '" role="button"><i
+            class="bi bi-folder"></i></a>
+    <button class="btn btn-danger delete-btn" href="' . route('destroy-mundurejo', $row->id) . '" role="button"><i
+            class="bi-trash3"></i></button>
+</div>
