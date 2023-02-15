c50d80a87f769e5cbc09ef41ffbeb3c8e97fcfd2
MeNavel <information.navel@gmail.com>
2023-02-15 07:26:18 +0700

    coba

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
         //Desa Pondok Joyo
         $dusun_pondokjoyo = Pondokjoyo::select('Dusun_Letak_Tanah')->groupBy('Dusun_Letak_Tanah')->get()->pluck('Dusun_Letak_Tanah')->all();
         $iter = 0;
-        while($iter<DB::table('pondokjoyo')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')){
+        while ($iter < DB::table('pondokjoyo')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')) {
             $data_dusun_pondokjoyo[$iter] = Pondokjoyo::select('*')->where('Dusun_Letak_Tanah', '=', $dusun_pondokjoyo[$iter])->count();
             $iter++;
         }
@@ -38,7 +38,7 @@ class HomeController extends Controller
         //Desa Mundurejo
         $dusun_mundurejo = Mundurejo::select('Dusun_Letak_Tanah')->groupBy('Dusun_Letak_Tanah')->get()->pluck('Dusun_Letak_Tanah')->all();
         $iter = 0;
-        while($iter<DB::table('mundurejo')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')){
+        while ($iter < DB::table('mundurejo')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')) {
             $data_dusun_mundurejo[$iter] = Mundurejo::select('*')->where('Dusun_Letak_Tanah', '=', $dusun_mundurejo[$iter])->count();
             $iter++;
         }
@@ -46,17 +46,10 @@ class HomeController extends Controller
         //Desa Sidomekar
         $dusun_sidomekar = Sidomekar::select('Dusun_Letak_Tanah')->groupBy('Dusun_Letak_Tanah')->get()->pluck('Dusun_Letak_Tanah')->all();
         $iter = 0;
-        while($iter<DB::table('sidomekar')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')){
+        while ($iter < DB::table('sidomekar')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')) {
             $data_dusun_sidomekar[$iter] = Sidomekar::select('*')->where('Dusun_Letak_Tanah', '=', $dusun_sidomekar[$iter])->count();
             $iter++;
         }
-        return view('home')->with(compact(
-            'dusun_pondokjoyo',
-            'data_dusun_pondokjoyo',
-            'dusun_mundurejo',
-            'data_dusun_mundurejo',
-            'dusun_sidomekar',
-            'data_dusun_sidomekar',
-        ));
+        return view('home', compact('dusun_pondokjoyo','data_dusun_pondokjoyo','dusun_mundurejo','data_dusun_mundurejo','dusun_sidomekar','data_dusun_sidomekar'));
     }
 }
