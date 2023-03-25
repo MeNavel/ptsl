2453ed637a9f9f4a5194fd6fc316d7137662c702
MeNavel <information.navel@gmail.com>
2023-03-25 10:26:46 +0700

    change blok to readonly

--- resources/views/mundurejo/create.blade.php
+++ resources/views/mundurejo/create.blade.php
@@ -22,9 +22,7 @@
                             </div>
                             <div class="col-md-3">
                                 <label for="Blok" class="form-label">Blok</label>
-                                <input type="text" name="Blok" class="form-control" id="Blok" value="000"
-                                    oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');">
-                                <span id="cek_Blok"></span>
+                                <input type="text" name="Blok" class="form-control" id="Blok" value="000" readonly>
                             </div>
                             <div class="col-md-3">
                                 <label for="No_SPPT" class="form-label">No SPPT</label>
