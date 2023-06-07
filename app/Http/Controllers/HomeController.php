687ae8050bce7ef607280b1528a051058487df57
MeNavel <information.navel@gmail.com>
2023-06-07 08:41:41 +0700

    delete data not used

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,46 +28,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $data_dusun_pondokjoyo[] = null;
-        $data_dusun_mundurejo[] = null;
-        $data_dusun_sumberagung[] = null;
-        $data_dusun_sidomekar[] = null;
-        $data_koordinator_pondokjoyo[] = null;
-        $data_koordinator_mundurejo[] = null;
-        //Desa Pondok Joyo
-        $dusun_pondokjoyo = Pondokjoyo::select('Dusun_Letak_Tanah')->groupBy('Dusun_Letak_Tanah')->get()->pluck('Dusun_Letak_Tanah')->all();
-        $iter = 0;
-        while ($iter < DB::table('pondokjoyo')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')) {
-            $data_dusun_pondokjoyo[$iter] = Pondokjoyo::select('*')->where('Dusun_Letak_Tanah', '=', $dusun_pondokjoyo[$iter])->count();
-            $iter++;
-        }
-
-        //Desa Mundurejo
-        $dusun_mundurejo = Mundurejo::select('Dusun_Letak_Tanah')->groupBy('Dusun_Letak_Tanah')->get()->pluck('Dusun_Letak_Tanah')->all();
-        $iter = 0;
-        while ($iter < DB::table('mundurejo')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')) {
-            $data_dusun_mundurejo[$iter] = Mundurejo::select('*')->where('Dusun_Letak_Tanah', '=', $dusun_mundurejo[$iter])->count();
-            $iter++;
-        }
-        // $all = $dusun_mundurejo->merge($data_dusun_mundurejo);
-        // dd(array_merge($dusun_mundurejo,$data_dusun_mundurejo));
-
-        //Desa Sumberagung
-        $dusun_sumberagung = Sumberagung::select('Dusun_Letak_Tanah')->groupBy('Dusun_Letak_Tanah')->get()->pluck('Dusun_Letak_Tanah')->all();
-        $iter = 0;
-        while ($iter < DB::table('sumberagung')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')) {
-            $data_dusun_sumberagung[$iter] = Sumberagung::select('*')->where('Dusun_Letak_Tanah', '=', $dusun_sumberagung[$iter])->count();
-            $iter++;
-        }
-
-        //Desa Sidomekar
-        $dusun_sidomekar = Sidomekar::select('Dusun_Letak_Tanah')->groupBy('Dusun_Letak_Tanah')->get()->pluck('Dusun_Letak_Tanah')->all();
-        $iter = 0;
-        while ($iter < DB::table('sidomekar')->select('Dusun_Letak_Tanah')->distinct()->count('Dusun_Letak_Tanah')) {
-            $data_dusun_sidomekar[$iter] = Sidomekar::select('*')->where('Dusun_Letak_Tanah', '=', $dusun_sidomekar[$iter])->count();
-            $iter++;
-        }
-
         //Koordinator Pondok Joyo
         $koordinator_pondokjoyo = Pondokjoyo::select('Nama_Saksi_1')->groupBy('Nama_Saksi_1')->get()->pluck('Nama_Saksi_1')->all();
         $iter = 0;
@@ -97,14 +57,6 @@ class HomeController extends Controller
         return view(
             'home',
             compact([
-                'dusun_pondokjoyo',
-                'data_dusun_pondokjoyo',
-                'dusun_mundurejo',
-                'data_dusun_mundurejo',
-                'dusun_sumberagung',
-                'data_dusun_sumberagung',
-                'dusun_sidomekar',
-                'data_dusun_sidomekar',
                 'koordinator_pondokjoyo',
                 'data_koordinator_pondokjoyo',
                 'koordinator_mundurejo',
